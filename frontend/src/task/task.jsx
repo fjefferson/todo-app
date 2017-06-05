8b2d80e52dab934289e49a8760e877bc39490aa8
Raphael Sá <raphaelbruno2.0@gmail.com>
2017-06-04 22:40:04 -0300

    Added Screenshot for preview.

--- frontend/src/task/task.jsx
+++ frontend/src/task/task.jsx
@@ -64,7 +64,7 @@ export default class Task extends Component {
         }
 
         Axios.post(URL, {description})
-            .then(response => this.refreshLists(this.state.description));
+            .then(response => this.refreshLists(''));
     }
 
     handleDoneToggle(item){
